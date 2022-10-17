f79a1dd2bd294a5b37149334f3ec4d7569f68fab
Kogane-Yakumo <51345842+Kogane-Yakumo@users.noreply.github.com>
2022-10-17 15:19:15 -0700

    Lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ * Author: Ganzorig Gantushig <ggantush@ucsc.edu>
+ * Created: 17 October
+ * License: Public Domain
+ */
+
+// Defining Variables
+make = "Toyota";
+model = "Highlander";
+color = "Grey";
+year = 2013;
+
+// Calculations
+age = 2022 - year;
+
+// Write Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
