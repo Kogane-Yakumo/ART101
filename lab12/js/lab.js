131cdae9d6603d6611a0ecd3eac5e2eb8483d414
Kogane-Yakumo <51345842+Kogane-Yakumo@users.noreply.github.com>
2022-11-08 14:54:50 -0800

    Lab 12 Bonus

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -6,23 +6,15 @@
 
 function sortingHat(str) {
     var length = str.length;
-    var mod = length % 4;
-    if (mod == 0) {
-        return "Gryffindor"
-    }
-    else if (mod == 1) {
-        return "Ravenclaw"
-    }
-    else if (mod == 2) {
-        return "Slytherin"
-    }
-    else {
-        return "Hufflepuff"
-    }
+    var houseArray = ["Gryffindor", "Ravenclaw", "Slytherin", "Hufflepuff"];
+    var descriptionArray = ["Gryffindor house holds the brave and the determined, its colors are scarlet and gold, and its symbol is the lion.", "Ravenclaw house holds the erudite and the wise, its colors are blue and bronze, and its symbol is the eagle.", "Slytherin house holds the cunning and the meticulous, its colors are green and silver, and its symbol is the serpent.", "Hufflepuff house holds the trustworthy and the diligent, its colors are yellow and black, and its symbol is the badger."];
+    var mod = length % houseArray.length;
+    return [houseArray[mod], descriptionArray[mod]]
 }
 
 $("#button").click(function(){
     var name = $("#input").val()
-    var house = sortingHat(name)
+    var house = sortingHat(name)[0]
     $("#output").append("The Sorting Hat has sorted you into " + house);
+    $("#output").append("<br>" + sortingHat(name)[1]);
 })
