fcaec3961288ff2c524cc4430cacc286e03f26d2
Kogane-Yakumo <51345842+Kogane-Yakumo@users.noreply.github.com>
2022-11-10 13:03:26 -0800

    Lab 13 Bonus

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -4,17 +4,34 @@
  * License: Public Domain
  */
 
-function FizzBuzz() {
-    for (var i=1; i<=200; i++){
+function getTableValues() {
+    table = []
+    for (var value=0; value<4; value++){
+        num = "#num" + value
+        text = "#text" + value
+        numVal = $(num).val()
+        textVal = $(text).val()
+        table.push([numVal,textVal])
+    }
+    console.log(table)
+    return table
+}
+
+function FizzBuzz(array, max) {
+    $("#output").html("")
+    for (var i=1; i<=max; i++){
         var string = ""
-        if (i % 3 == 0) {
-            string = string + "Fizz"
+        if (i % array[0][0] == 0) {
+            string = string + array[0][1]
         }
-        if (i % 5 == 0) {
-            string = string + "Buzz"
+        if (i % array[1][0] == 0) {
+            string = string + array[1][1]
         }
-        if (i % 7 == 0) {
-            string = string + "Boom"
+        if (i % array[2][0] == 0) {
+            string = string + array[2][1]
+        }
+        if (i % array[3][0] == 0) {
+            string = string + array[3][1]
         }
         string = string + "!"
         if (string !== "!") {
@@ -27,5 +44,7 @@ function FizzBuzz() {
 }
 
 $("#my-button").click(function(){
-    FizzBuzz()
+    var maximum = $("#max").val()
+    console.log(maximum)
+    FizzBuzz(getTableValues(), maximum)
 });
