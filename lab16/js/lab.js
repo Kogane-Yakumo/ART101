5b55ae75c4ccd24d6fe57b31a8561ed1d6d3992e
Kogane-Yakumo <51345842+Kogane-Yakumo@users.noreply.github.com>
2022-11-28 22:38:52 -0800

    16 Bonus

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -4,22 +4,42 @@
  * License: Public Domain
  */
 
-$("#activate").click(function () {
+var domain = "https://xkcd.com/";
+var linkEnd = "info.0.json";
+
+function comicGen(number) {
+    if (number == undefined) {
+        comicNum = "";
+    } else {
+        comicNum = number.toString() + "/";
+    }
+    var URL = domain + comicNum + linkEnd;
     $.ajax({
-        url: "https://xkcd.com/info.0.json",
+        url: URL,
         type: "GET",
         dataType: "json",
-        success: function(data) {
+        success: function (data) {
             $("#output").html("")
             var comicObj = data;
             $("#output").append("<h1> " + comicObj.title + ".");
             $("#output").append("<img src=" + comicObj.img + " title=" + comicObj.alt + ">");
+            $("#output").append('<p> Wanna keep reading? <br>');
+            $("#output").append('<button id="prev">Previous</button>');
+            $("#output").append('<button id="next">Next</button>');
+            $("#prev").click(function(){
+                comicGen(comicObj.num - 1);
+            });
+            $("#next").click(function(){
+                comicGen(comicObj.num - 1);
+            });
             console.log(JSON.stringify(comicObj, null, 2));
         },
-        error: function (jqXHR, textStatus, errorThrown) { 
+        error: function (jqXHR, textStatus, errorThrown) {
             $("#output").html("")
             $("#output").append("An execption has occurred.")
             console.log("Error:", textStatus, errorThrown);
         }
     })
-})
+}
+
+comicGen()
